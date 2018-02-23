f989684c10fc891dc7e75784899000ab8608c26d
mherrington8944 <mherrington8944@conestogac.on.ca>
2018-02-22 21:02:03 -0500

    Made it to the css portion (almost the end) of the tutorial

--- TodoApi/TodoApi/Startup.cs
+++ TodoApi/TodoApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
 
 namespace TodoApi
 {
@@ -19,10 +21,39 @@ namespace TodoApi
 		{
 			services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 			services.AddMvc();
+
+			//register the swagger generator, defining one or more swagger documents
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info {
+					Title = "My API",
+					Version = "v1",
+					Description= "A simple ASP.Net Core Web API I made by following a tutorial",
+					TermsOfService="None",
+					Contact= new Contact { Name = "Max Herrington", Url = "https://github.com/mherrington8944" }
+				});
+
+				//set the comments path for the swagger json and UI
+				var basePath = AppContext.BaseDirectory;
+				var xmlPath = Path.Combine(basePath, "TodoApi.xml");
+				c.IncludeXmlComments(xmlPath);
+			});
+
 		}
 
 		public void Configure(IApplicationBuilder app)
 		{
+			app.UseStaticFiles();
+
+			//enable middleware to serve generated swagger as a JSON endpoint
+			app.UseSwagger();
+
+			//enable middleware to serve swagger-ui (htmls, js, css, etc.), specifying the json endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			});
+
 			app.UseMvc();
 		}
 	}
